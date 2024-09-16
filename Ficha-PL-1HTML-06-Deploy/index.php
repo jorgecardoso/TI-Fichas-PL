73b2001d0528b4aaaf7d620ee1d36e46d21449fc
Jorge C. S. Cardoso <jorgecardoso@dei.uc.pt>
2024-09-16 11:57:53 +0100

    Corrigida mensagem nos index.php das fichas.

--- Ficha-PL-1HTML-06-Deploy/index.php
+++ Ficha-PL-1HTML-06-Deploy/index.php
@@ -4,21 +4,21 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
-  <title>Ficha PL HTML 05 - Tabelas</title>
+  <title>Ficha PL HTML 06 - Deploy</title>
    <?php include('../.includes/css-html.html') ?>
 </head>
 <body>
  <!-- Este é o documento index.php e deve ficar inalterado durante a resolução da ficha. -->
   <header>
-    <h1>HTML: Tabelas</h1>
+    <h1>HTML: Deploy</h1>
   </header>
   <main>
     <div>
       <ol>
         <li><a href="ex1.html">ex1.html</a></li>
-        <li><a href="ex2.html">ex2.html</a></li>
+       <!-- <li><a href="ex2.html">ex2.html</a></li>
         <li><a href="ex3.html">ex3.html</a></li>
-       <!-- <li><a href="ex4.html">ex4.html</a></li>
+       <li><a href="ex4.html">ex4.html</a></li>
         <li><a href="ex5.html">ex5.html</a></li>
         <li><a href="ex6.html">ex6.html</a></li>
         <li><a href="ex7.html">ex7.html</a></li>
